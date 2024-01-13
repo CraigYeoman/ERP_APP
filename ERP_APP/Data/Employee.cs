765ada64fd6fa543763b03ea78501527b4d76371
Craig Yeoman <yeomancraig@outlook.com>
2024-01-13 15:08:27 -0600

    Add project files.

---
+++ ERP_APP/Data/Employee.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Security.Cryptography.X509Certificates;
+
+namespace ERP_APP.Data
+{
+    public class Employee : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
