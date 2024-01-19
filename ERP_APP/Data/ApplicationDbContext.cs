d0a5bef49e83d5f9c42b1b15f32413c582b94bf9
Craig Yeoman <yeomancraig@outlook.com>
2024-01-19 12:25:23 -0600

    CSS Layout changes

--- ERP_APP/Data/ApplicationDbContext.cs
+++ ERP_APP/Data/ApplicationDbContext.cs
@@ -9,16 +9,32 @@ namespace ERP_APP.Data
             : base(options)
         {
         }
-        public DbSet<BaseEntity> BaseEntities { get; set; }
-        public DbSet<WorkOrder> WorkOrders { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<JobType> JobTypes { get; set; }
-        public DbSet<Accessories> Accessories { get; set; }
-        public DbSet<Part> Parts { get; set; }
-        public DbSet<Vendor> Vendors { get; set; }
-        public DbSet<PartCategory> PartCategories { get; set; }
-        public DbSet<Labor> Labors { get; set; }
-        public DbSet<LaborCategory> LaborCategories { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in base.ChangeTracker.Entries<BaseEntity>().Where(q => q.State == EntityState.Added
+            || q.State == EntityState.Modified))
+            {
+                entry.Entity.DateModified = DateTime.Now;
+
+                if (entry.State == EntityState.Added)
+                {
+                    entry.Entity.DateCreated = DateTime.Now;
+                }
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+
+        public DbSet<BaseEntity>? BaseEntities { get; set; }
+        public DbSet<WorkOrder>? WorkOrders { get; set; }
+        public DbSet<Customer>? Customers { get; set; }
+        public DbSet<JobType>? JobTypes { get; set; }
+        public DbSet<Accessories>? Accessories { get; set; }
+        public DbSet<Part>? Parts { get; set; }
+        public DbSet<Vendor>? Vendors { get; set; }
+        public DbSet<PartCategory>? PartCategories { get; set; }
+        public DbSet<Labor>? Labors { get; set; }
+        public DbSet<LaborCategory>? LaborCategories { get; set; }
 
     }
 }
